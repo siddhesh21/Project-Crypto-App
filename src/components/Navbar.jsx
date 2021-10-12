be361bc69110b20f8460e38588edcfbbfa908ff5
Siddhesh Kankekar <kankekas@tcd.ie>
2021-10-12 06:42:18 +0100

    Initial setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  BulbOutlined,
+  MoneyCollectOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+
+const Navbar = () => {
+  return <div></div>;
+};
+
+export default Navbar;
